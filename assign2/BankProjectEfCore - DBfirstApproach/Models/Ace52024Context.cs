721cf2f0f7b3b11a69f853634a7b1f25f202a880
Kartik Gupta <73742840+CoderKartik-1@users.noreply.github.com>
2024-01-16 12:33:05 +0530

    DB-first approach update
    Added context.cs and model files for Bank Project using DB first approach.

---
+++ assign2/BankProjectEfCore - DBfirstApproach/Models/Ace52024Context.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace BankProjectEfCore.Models;
+
+public partial class Ace52024Context : DbContext
+{
+    public Ace52024Context()
+    {
+    }
+
+    public Ace52024Context(DbContextOptions<Ace52024Context> options)
+        : base(options)
+    {
+    }
+
+    
+
+    public virtual DbSet<KartikSbaccount> KartikSbaccounts { get; set; }
+
+    public virtual DbSet<KartikSbtransaction> KartikSbtransactions { get; set; }
+
+    
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=DEVSQL.Corp.local;Database=ACE 5- 2024;Trusted_Connection=True; encrypt=false;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+
+        modelBuilder.Entity<KartikSbaccount>(entity =>
+        {
+            entity.HasKey(e => e.AccountNumber).HasName("PK__KartikSB__BE2ACD6E20127B65");
+
+            entity.ToTable("KartikSBAccount");
+
+            entity.Property(e => e.AccountNumber).ValueGeneratedNever();
+            entity.Property(e => e.CurrentBalance).HasColumnType("decimal(18, 2)");
+            entity.Property(e => e.CustomerAddress)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.CustomerName)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<KartikSbtransaction>(entity =>
+        {
+            entity.HasKey(e => e.TransactionId).HasName("PK__KartikSB__55433A6BD3049A65");
+
+            entity.ToTable("KartikSBTransaction");
+
+            entity.Property(e => e.TransactionId).ValueGeneratedNever();
+            entity.Property(e => e.Amount).HasColumnType("decimal(18, 2)");
+            entity.Property(e => e.TransactionDate).HasColumnType("datetime");
+            entity.Property(e => e.TransactionType)
+                .HasMaxLength(15)
+                .IsUnicode(false);
+
+            entity.HasOne(d => d.AccountNumberNavigation).WithMany(p => p.KartikSbtransactions)
+                .HasForeignKey(d => d.AccountNumber)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__KartikSBT__Accou__12FDD1B2");
+        });
+
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ assign2/BankProjectEfCore - DBfirstApproach/Program.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using BankProjectEfCore.Models;
+using Microsoft.EntityFrameworkCore.Infrastructure.Internal;
+
+
+namespace Bank{
+    class Bank_Functions{
+        private static Ace52024Context db = new Ace52024Context();
+        //Transaction ID counter
+        public static int Tid = 0;
+
+        //Add Account function
+        public static void AddAccount(){
+            Console.WriteLine();
+            KartikSbaccount sb = new KartikSbaccount();
+            Console.WriteLine("Enter account number of account to be added: ");
+            int acc;
+            bool check = int.TryParse(Console.ReadLine(), out acc);
+            if(!check){
+                throw new AccountNotValidException("Account number not in correct format, only input integers!");
+            }
+            Console.WriteLine("Enter customer name: ");
+            sb.CustomerName = Console.ReadLine();
+            Console.WriteLine("Enter customer address: ");
+            sb.CustomerAddress = Console.ReadLine();
+            Console.WriteLine("Enter current balance: ");
+            sb.CurrentBalance = decimal.Parse(Console.ReadLine());
+            sb.AccountNumber = acc;
+            db.KartikSbaccounts.Add(sb);
+            db.SaveChanges();
+            Console.WriteLine("Account added");
+        }
+
+        //Deposit Amount function
+        public static void DepositAmount(int accNo, decimal amt){
+            Console.WriteLine();
+            KartikSbaccount sb = null;
+            foreach(var item in db.KartikSbaccounts){
+                if(item.AccountNumber==accNo){
+                    sb = item;
+                    break;
+                }
+            }
+            if(sb!=null){
+                sb.CurrentBalance += amt;
+                Console.WriteLine($"{amt} deposited in {sb.CustomerName} 's account");
+                Console.WriteLine($"Available balance now is {sb.CurrentBalance}");
+                //Add transaction to transactions table
+                Tid++;
+                KartikSbtransaction tr = new KartikSbtransaction();
+                tr.TransactionId = Tid;
+                tr.TransactionDate = DateTime.Now;
+                tr.AccountNumber = accNo;
+                tr.Amount = amt;
+                tr.TransactionType = "Deposit";
+                db.KartikSbtransactions.Add(tr);
+                db.SaveChanges();
+            }
+            else{
+                throw new AccountNotValidException("Account number not in database");
+            }
+        }
+
+        //Withdraw function
+        public static void WithdrawAmount(int accNo, decimal amt){
+            Console.WriteLine();
+            KartikSbaccount sb = null;
+            foreach(var item in db.KartikSbaccounts){
+                if(item.AccountNumber==accNo){
+                    sb = item;
+                    break;
+                }
+            }
+            if(sb!=null){
+                if(amt<=sb.CurrentBalance){
+                    sb.CurrentBalance -= amt;
+                    Console.WriteLine($"{amt} withdrawed from {sb.CustomerName} 's account");
+                    Console.WriteLine($"Available balance now is {sb.CurrentBalance}");
+                    Tid++;
+                    KartikSbtransaction tr = new KartikSbtransaction();
+                    tr.TransactionId = Tid;
+                    tr.TransactionDate = DateTime.Now;
+                    tr.AccountNumber = accNo;
+                    tr.Amount = amt;
+                    tr.TransactionType = "Withdraw";
+                    db.KartikSbtransactions.Add(tr);
+                    db.SaveChanges();
+                }
+                else{
+                    throw new InsufficientBalanceException("Insufficent balance, cannot withdraw!");
+                }
+            }
+            else{
+                throw new AccountNotValidException("Account number not in database");
+            }
+        }
+
+        //Get AccountDetails function
+        public static void GetAccountDetails(int accNo){
+            Console.WriteLine();
+            KartikSbaccount sb = null;
+            foreach(var item in db.KartikSbaccounts){
+                if(item.AccountNumber == accNo){
+                    sb = item;
+                    break;
+                }
+            }
+            if(sb==null){
+                throw new AccountNotValidException("Account number provided not valid!");;
+            }
+            Console.WriteLine($"Account details with {accNo} Account Number: ");
+            Console.WriteLine(sb.AccountNumber + " " + sb.CustomerName + " " + sb.CustomerAddress + " " + sb.CurrentBalance);
+        } 
+
+        //Get All Accounts
+        public static void GetAllAccounts(){
+            Console.WriteLine();
+            Console.WriteLine("All Accounts: ");
+            foreach(var item in db.KartikSbaccounts){
+                Console.WriteLine($"{item.AccountNumber} | {item.CustomerName} | {item.CustomerAddress} | {item.CurrentBalance}");
+            }
+        }
+
+        public static void GetAllTransactions(int accNo){
+            Console.WriteLine();
+            KartikSbaccount sb = null;
+            foreach(var item in db.KartikSbaccounts){
+                if(item.AccountNumber == accNo){
+                    sb = item;
+                    break;
+                }
+            }
+            if(sb==null){
+                throw new AccountNotValidException("Account number provided not valid!");;
+            }
+            else{
+                Console.WriteLine($"All transactions for given account number {accNo} -");
+                foreach(var item in db.KartikSbtransactions){
+                    if(item.AccountNumber==accNo){
+                        Console.WriteLine($"{item.TransactionId} | {item.TransactionDate} | {item.AccountNumber} | {item.Amount} | {item.TransactionType}");
+                    }
+                }
+            }
+        }
+
+        //Main Program
+        public static void Main(){
+            try{
+                AddAccount();
+                AddAccount();
+                DepositAmount(1, 5000);
+                DepositAmount(2, 10000);
+                GetAllTransactions(2);
+            }
+            catch(AccountNotValidException e){
+                Console.WriteLine(e.Message);
+            }
+            catch(InsufficientBalanceException e){
+                Console.WriteLine(e.Message);
+            }
+            catch(Exception e){
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
